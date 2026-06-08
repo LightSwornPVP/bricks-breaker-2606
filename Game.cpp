7b83c679a7d41483b002f773f9156da517c7cd93
LightSwornPVP <lightsworn@uniquin-pvp.com>
2026-06-07 23:22:09 -0400

    1.4 complete

--- Game.cpp
+++ Game.cpp
@@ -1,127 +1,147 @@
 #include "stdafx.h"
 #include "Game.h"
+#include <iostream>
 
 Game::Game()
 {
-	Reset();
+    Reset();
 }
 
 void Game::Reset()
 {
-	Console::SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	Console::CursorVisible(false);
-	paddle.width = 12;
-	paddle.height = 2;
-	paddle.x_position = 32;
-	paddle.y_position = 30;
+    Console::SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    Console::CursorVisible(false);
 
-	ball.visage = 'O';
-	ball.color = ConsoleColor::Cyan;
-	ResetBall();
+    paddle.width = 12;
+    paddle.height = 2;
+    paddle.x_position = 32;
+    paddle.y_position = 30;
 
-	// TODO #2 - Add this brick and 4 more bricks to the vector
-	bricks.clear();
+    ball.visage = 'O';
+    ball.color = ConsoleColor::Cyan;
+    ResetBall();
 
-	for (int i = 0; i < 5; ++i)
-	{
+    gameWon = false;
+    gameLost = false;
 
-		Box brick;
-		brick.width = 10;
-		brick.height = 2;
-		brick.x_position = 0;
-		brick.y_position = 5;
-		brick.doubleThick = true;
-		brick.color = ConsoleColor::DarkGreen;
+    // TODO #2 - Add this brick and 4 more bricks to the vector
+    bricks.clear();
 
-		bricks.push_back(brick);
-	}
+    for (int i = 0; i < 5; ++i)
+    {
+        Box brick;
+        brick.width = 10;
+        brick.height = 2;
+        brick.x_position = 5 + (i * 14);
+        brick.y_position = 5;
+        brick.doubleThick = true;
+        brick.color = ConsoleColor::DarkGreen;
+
+        bricks.push_back(brick);
+    }
 }
 
 void Game::ResetBall()
 {
-	ball.x_position = paddle.x_position + paddle.width / 2;
-	ball.y_position = paddle.y_position - 1;
-	ball.x_velocity = rand() % 2 ? 1 : -1;
-	ball.y_velocity = -1;
-	ball.moving = false;
+    ball.x_position = paddle.x_position + paddle.width / 2;
+    ball.y_position = paddle.y_position - 1;
+    ball.x_velocity = rand() % 2 ? 1 : -1;
+    ball.y_velocity = -1;
+    ball.moving = false;
 }
 
 bool Game::Update()
 {
-	if (GetAsyncKeyState(VK_ESCAPE) & 0x1)
-		return false;
+    if (GetAsyncKeyState(VK_ESCAPE) & 0x1)
+        return false;
 
-	if (GetAsyncKeyState(VK_RIGHT) && paddle.x_position < WINDOW_WIDTH - paddle.width)
-		paddle.x_position += 2;
+    if (GetAsyncKeyState(VK_RIGHT) && paddle.x_position < WINDOW_WIDTH - paddle.width)
+        paddle.x_position += 2;
 
-	if (GetAsyncKeyState(VK_LEFT) && paddle.x_position > 0)
-		paddle.x_position -= 2;
+    if (GetAsyncKeyState(VK_LEFT) && paddle.x_position > 0)
+        paddle.x_position -= 2;
 
-	if (GetAsyncKeyState(VK_SPACE) & 0x1)
-		ball.moving = !ball.moving;
+    if (GetAsyncKeyState(VK_SPACE) & 0x1)
+        ball.moving = !ball.moving;
 
-	if (GetAsyncKeyState('R') & 0x1)
-		Reset();
+    if (GetAsyncKeyState('R') & 0x1)
+        Reset();
 
-	ball.Update();
-	CheckCollision();
-	return true;
+    if (!gameWon && !gameLost)
+    {
+        ball.Update();
+        CheckCollision();
+    }
+
+    return true;
 }
 
-//  All rendering, including text, should occur in the Render function
+// All rendering, including text, should occur in the Render function
 void Game::Render() const
 {
-	Console::Lock(true);
-	Console::Clear();
-	
-	paddle.Draw();
-	ball.Draw();
+    Console::Lock(true);
+    Console::Clear();
 
-	// TODO #3 - Update render to render all bricks
-	
+    paddle.Draw();
+    ball.Draw();
 
-	for (int i = 0; i < bricks.size(); ++i)
-	{
-		bricks[i].Draw();
-	}
+    // TODO #3 - Update render to render all bricks
+    for (int i = 0; i < bricks.size(); ++i)
+    {
+        bricks[i].Draw();
+    }
 
-	Console::Lock(false);
+    if (gameWon)
+    {
+        Console::SetCursorPosition(25, 15);
+        std::cout << "You win! Press 'R' to play again.";
+    }
+
+    if (gameLost)
+    {
+        Console::SetCursorPosition(25, 15);
+        std::cout << "You lose. Press 'R' to play again.";
+    }
+
+    Console::Lock(false);
 }
-
 
 void Game::CheckCollision()
 {
-	// TODO #4 - Update collision to check all bricks
-	
-	for (int i = 0; i < bricks.size(); ++i)
-	{
+    // TODO #4 - Update collision to check all bricks
+    for (int i = 0; i < bricks.size(); ++i)
+    {
+        if (bricks[i].Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+        {
+            bricks[i].color = ConsoleColor(bricks[i].color - 1);
+            ball.y_velocity *= -1;
 
-		if (bricks[i].Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
-		{
-			bricks[i].color = ConsoleColor(bricks[i].color - 1);
-			ball.y_velocity *= -1;
+            // TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
+            if (bricks[i].color == ConsoleColor::Black)
+            {
+                bricks.erase(bricks.begin() + i);
+            }
 
-			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-			if (bricks[i].color == ConsoleColor::Black)
-			{
-				bricks.erase(bricks.begin() + i);
-			}
+            break;
+        }
+    }
 
-			break;
-		}
-	}
-	// TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
+    // TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
+    if (bricks.empty())
+    {
+        ball.moving = false;
+        gameWon = true;
+    }
 
+    if (paddle.Contains(ball.x_position + ball.x_velocity, ball.y_velocity + ball.y_position))
+    {
+        ball.y_velocity *= -1;
+    }
 
-	if (paddle.Contains(ball.x_position + ball.x_velocity, ball.y_velocity + ball.y_position))
-	{
-		ball.y_velocity *= -1;
-	}
-
-	// TODO #7 - If ball touches bottom of window, pause ball and display (render) defeat text with R to reset
-	if (ball.y_position >= WINDOW_HEIGHT - 1)
-	{
-		ball.moving = false;
-		gameLost = true;
-	}
+    // TODO #7 - If ball touches bottom of window, pause ball and display (render) defeat text with R to reset
+    if (ball.y_position >= WINDOW_HEIGHT - 1)
+    {
+        ball.moving = false;
+        gameLost = true;
+    }
 }
